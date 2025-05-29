2cbc65662f71ed02a4e5c118d0b2070bc13cbc6a
siddharth ashri <siddharth.ashri112@gmail.com>
2025-05-28 19:19:34 -0700

    enabled raw mode and added timeout for reading input

--- kilo.c
+++ kilo.c
@@ -1,27 +1,63 @@
+/***includes ***/
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+/*** data ***/
 struct termios orig_termios;
 
-void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
+/*** terminal ***/
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
+void disableRawMode() {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
+}
 
 void enableRawMode() {
     // get the original attributes and set them to orig_termios
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+        die("tcgetattr");
+    }
     atexit(disableRawMode);
     struct termios raw = orig_termios;
     tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO);
-
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    // disable CR, NL, CTRL-S and CTRL-Q input flags - we're turning off this
+    // via bit flipping
+    raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
+    // disable echo, and  CTRL-V and CTRL-C, CTRL-Z
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    // we want 8 bits per byte of character
+    raw.c_cflag |= (CS8);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 10;
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        die("tcsetattr");
+    }
 }
 
+/*** init ***/
 int main() {
     enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    while (1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+            die("read");
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if (c == 'q')
+            break;
     };
     return 0;
 }
