4961c09ec4bc11200cd6741510cbda771d5943d8
siddharth ashri <siddharth.ashri112@gmail.com>
2025-06-02 17:26:48 -0700

    get terminal screen rows and colums

--- kilo.c
+++ kilo.c
@@ -1,7 +1,10 @@
 /***includes ***/
+#include <asm-generic/ioctls.h>
+#include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -11,6 +14,8 @@
 
 /*** data ***/
 struct editorConfig {
+    int screenrows;
+    int screencols;
     struct termios orig_termios;
 };
 
@@ -38,6 +43,18 @@ char editorReadKey() {
             die("read");
     }
     return c;
+}
+
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        return -1;
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
 }
 
 /**** output  ****/
@@ -93,8 +110,15 @@ void enableRawMode() {
 }
 
 /*** init ***/
+
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+        die("getWindowSize");
+}
+
 int main() {
     enableRawMode();
+    initEditor();
 
     while (1) {
         editorRefreshScreen();
